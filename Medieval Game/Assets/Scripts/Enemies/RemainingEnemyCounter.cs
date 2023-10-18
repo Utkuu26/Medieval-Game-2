cc4e2f391ddb618e1b38020c5d8e50e474b3b7bf
Utku Gezensoy <utkugezensoy@gmail.com>
2023-10-18 19:58:45 +0300

    Final commit

--- Medieval Game/Assets/Scripts/Enemies/RemainingEnemyCounter.cs
+++ Medieval Game/Assets/Scripts/Enemies/RemainingEnemyCounter.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class RemainingEnemyCounter : MonoBehaviour
@@ -18,7 +19,14 @@ public class RemainingEnemyCounter : MonoBehaviour
     {
         if (remainingEnemy.text== "Remaining Enemy: 0")
         {
-            missionText.text = "Village saved successfully thanks to you";
+           StartCoroutine(Congrats(3f)); 
         }
+    }
+
+    private IEnumerator Congrats(float waitTime)
+    {
+        missionText.text = "Village saved successfully thanks to you";
+       yield return new WaitForSeconds(waitTime);
+       SceneManager.LoadScene(3);
     }
 }
